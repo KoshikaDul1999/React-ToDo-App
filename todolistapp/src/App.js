f31fffc662667dac1815e604c36302b4994b4745
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-04-15 11:28:15 +0530

    create todo form

--- todolistapp/src/App.js
+++ todolistapp/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>ToDo App</h1>
+      <TodoForm />
     </div>
   );
 }
