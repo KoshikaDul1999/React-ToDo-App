30a85fb95a55b853545daf15597b1a7837157989
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-04-21 14:14:05 +0530

    updated

--- todolistapp/src/components/TodoForm.js
+++ todolistapp/src/components/TodoForm.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
     
 const [input, setInput] = useState('');
+
+const inputRef = useRef(null)
+
+useEffect(() => {
+    inputRef.current.focus()
+})
 
 const handleChange = e => {
     setInput(e.target.value);
@@ -27,6 +33,7 @@ const handleSubmit = e => {
             value={input} name='text' 
             className='todo-input' 
             onChange={handleChange}
+            ref={inputRef}
         />
         <button className='todo-button'>Add ToDo</button>
     </form>
